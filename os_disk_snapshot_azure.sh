6a4059c88b5bc03d292018d6d2e0014fd1532c40
ShanuShan <shanu.ansari.08@gmail.com>
2021-06-14 12:33:40 -0400

    Updated checking the name of vm already exist before taking the snapshot

--- os_disk_snapshot_azure.sh
+++ os_disk_snapshot_azure.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 read -p "Provide VM name:" vmn
+if [ "$(az vm list -d -o table --query "[?name=='$vmn']")" = "" ];
+then
+    echo "No VM was found."
+    exit
+fi
 rg=`az vm list -o table| grep -i $vmn | awk '{print $2}'`
 osdisk=`az vm show --resource-group $rg -n $vmn --query "storageProfile.osDisk.managedDisk.id" | grep -oP 'disks/\K.+' | rev|cut -c2- |rev`
 snap=`az snapshot list --query "[].name" | grep -w "${vmn}_os_snapshot_patching" | tr -d \" | tr -d ,|sed -e 's/^[ \t]*//'`
-echo $snap
 if [[ $snap = "${vmn}_os_snapshot_patching" ]]
 then
-  echo "exist"
+  echo "Snapshot already exist"
   echo "Below are the snapshot(s) already exist:"
   for dsnap in `az snapshot list --query "[].name" | grep -i $vmn | tr -d \" | tr -d ,|sed -e 's/^[ \t]*//'`
   do
